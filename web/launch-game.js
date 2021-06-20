3dd2d40d6d4fcdf61b6964d019e5853456df0d1e
Rupesh Dhadiwal <dhadiwalrupesh7@gmail.com>
2021-06-21 02:17:21 +0530

    changes

--- web/launch-game.js
+++ web/launch-game.js
@@ -2,13 +2,13 @@ function launchGame(name) {
     var uid = location.href.split('?')[1];
     var gameurl="";
     if(name=='car-racing' ){
-        var gameurl="https://vitaracegame.web.app?uid="+uid;
+        var gameurl="https://vitaracegame.web.app?"+uid;
     }
     if(name=='running-game'){
-        var gameurl="https://vitasoygogreen.web.app?uid="+uid;
+        var gameurl="https://vitasoygogreen.web.app?"+uid;
     }
     if(name=='photo-hunter'){
-        var gameurl="https://vitaphotohunt.web.app?uid="+uid;
+        var gameurl="https://vitaphotohunt.web.app?"+uid;
     }
      console.log("params", uid);
      window.open(gameurl, "_blank", "width="+screen.availWidth+",height="+screen.availHeight);
